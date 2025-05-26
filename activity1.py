557be0693439a163057ff7601bc44827729cfd17
iframaster <ifraanjum.codingal@gmail.com>
2025-05-26 13:13:16 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,14 @@
+#storing values
+score1=98
+score2=100
+score3=78
+score4=55
+score5=100
+
+#total number of scores
+sum=score1+score2+score3+score4+score5
+print("the sum of all 5 subjects is : {}".format(sum))
+
+#finding the average of score
+average=sum/5
+print("the Average of all 5 subjects is : {}".format(average))
